30e16b7a3fd0c6a23942cdab11d00769261e21f3
Rooshan <rooshan.ahmed@hotmail.com>
2020-11-27 00:54:42 +0500

    About.tsx Updated

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,16 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { navigate } from "gatsby"
 
 export default function About(){
     return(
       <Layout headerTitle="About Page Header">
         <div>
           About Page!
+          <br />
+          <button onClick={() => {
+            navigate("/")
+          }}>Back To Home</button>
         </div>
       </Layout>
     )
