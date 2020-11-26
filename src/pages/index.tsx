c6e7526c8ed07ab2cb5557fc0259e3d31380a327
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-11-27 00:00:59 +0500

    index.tsx updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import React from "react"
+import { Link, navigate } from "gatsby"
+
 
 export default function Home() {
   return(
     <div>
       Hello world!
-      
+      <br />
+      <Link to='/about'>About</Link>
+      <br />
+      <button onClick={() => {
+        navigate("/about")
+      }}>About Page Button</button>
     </div>
   ) 
 }
