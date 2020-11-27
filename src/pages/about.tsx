dbd737a63cf85f55bf6690422b3afba1f6aa5201
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-11-27 21:14:01 +0500

    App Updated

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { navigate } from "gatsby"
+import styles from "./about.module.css"
 
 export default function About(){
     return(
       <Layout headerTitle="About Page Header">
         <div>
-          About Page!
+          <h1 className={styles.myTitle}>About Page!</h1>
           <br />
           <button onClick={() => {
             navigate("/")
